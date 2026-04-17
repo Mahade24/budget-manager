7ddb362cd68ef7c8c60a78879b6cb9bf0993da43
Mahade24 <mahade.official@gmail.com>
2026-04-17 23:16:01 +0800

    Add Firebase initialization in firebase.js

---
+++ scr/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVWJyFF7GXtyPu2MZFfnEr54hwczWq7LM",
+  authDomain: "budget-manager-d6987.firebaseapp.com",
+  projectId: "budget-manager-d6987",
+  storageBucket: "budget-manager-d6987.firebasestorage.app",
+  messagingSenderId: "203917985342",
+  appId: "1:203917985342:web:7152a588395d6a84e2f36f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
